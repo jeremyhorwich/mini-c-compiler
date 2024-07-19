5b723b85af7611c968d0000d7677ff36d93c5481
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-07-19 15:42:56 -0500

    Create ASTNodes.cs

---
+++ Compiler/parser/ASTNodes.cs
@@ -0,0 +1,79 @@
+using Lex;
+
+namespace Parser
+{
+    abstract class Node
+    {
+        public List<Token> validTokens = new List<Token>();
+
+        public Node(List<Token> tokens)
+        {
+            validTokens = Parse(tokens);
+        }
+
+        public abstract List<Token> Parse(List<Token> tokens);
+        public abstract string Generate();
+    }
+
+
+    class Expression : Node
+    {
+        public Expression(List<Token> tokens) : base(tokens)
+        {
+        }
+
+        public override List<Token> Parse(List<Token> tokens)
+        {
+            if (tokens[0].Type == TokenType.integerLiteral)
+            {
+                return [tokens[0]];
+                //Must change to match regular expressions (integers of more than one digit)
+            }
+
+            return [];
+        }
+
+        public override string Generate()
+        {
+            return "";
+        }
+    }   
+
+    class Return : Node
+    {
+        public Return(List<Token> tokens) : base(tokens)
+        {
+        }
+
+        public override List<Token> Parse(List<Token> tokens)
+        {
+            if (tokens.Count < 3)
+            {
+                return [];
+            }
+
+            if (tokens[0].Value != "return")
+            {
+                return [];
+            }
+
+            if (tokens[1].Type != TokenType.integerLiteral) 
+            {
+                return [];
+                //Must modify indices for multidigit integers
+            }
+            
+            if (tokens[2].Type != TokenType.semicolon)
+            {
+                return [];
+            }
+
+            return tokens.GetRange(0, 3);
+        }
+
+        public override string Generate()
+        {
+            return "";
+        }
+    }
+}
