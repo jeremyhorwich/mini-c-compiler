64941070494faa79645fb2c99dad8e82d32e1027
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-07-25 15:31:27 -0500

    include basic parsing functions

--- Compiler/parser/Parsers.cs
+++ Compiler/parser/Parsers.cs
@@ -4,21 +4,31 @@ namespace Parse
 {
     public static class Parsers
     {
+        private static bool CheckTokenValue(IEnumerator<Token> tokens, string value)
+        {
+            return !tokens.MoveNext() || tokens.Current.Value == value;
+        }
+        
+        private static bool CheckTokenType(IEnumerator<Token> tokens, TokenType tType)
+        {
+
+            return !tokens.MoveNext() || tokens.Current.Type == tType;
+        }
+
         public static Function? ParseFunction(IEnumerator<Token> tokens)
         {
-            if (!tokens.MoveNext() || tokens.Current.Value != "int") return null;
-            
-            if (!tokens.MoveNext() || tokens.Current.Type == TokenType.identifier) return null;
+            if (!CheckTokenValue(tokens, "int")) return null;
+            if (!CheckTokenType(tokens, TokenType.identifier)) return null;
             string identifier = tokens.Current.Value;
 
-            if (!tokens.MoveNext() || tokens.Current.Type != TokenType.openParantheses) return null;
-            if (!tokens.MoveNext() || tokens.Current.Type != TokenType.closedParentheses) return null;
-            if (!tokens.MoveNext() || tokens.Current.Type != TokenType.openBrace) return null;
-            
+            if (!CheckTokenType(tokens, TokenType.openParantheses)) return null;
+            if (!CheckTokenType(tokens, TokenType.closedParentheses)) return null;
+            if (!CheckTokenType(tokens, TokenType.openBrace)) return null;
+           
             Statement? statement = ParseStatement(tokens);
             if (statement is null) return null;
 
-            if (!tokens.MoveNext() || tokens.Current.Type != TokenType.closeBrace) return null;
+            if (!CheckTokenType(tokens, TokenType.closeBrace)) return null;
             
             return new Function(identifier, statement);
         }
