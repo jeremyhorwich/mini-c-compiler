ddf7bd3f8750a192ae1a6ac4cd5e6770b8e05370
jeremyhorwich <jeremyahorwich@gmail.com>
2024-07-25 19:09:44 -0500

    rework parser into classes

--- Compiler/parser/Parsers.cs
+++ Compiler/parser/Parsers.cs
@@ -1,7 +1,84 @@
 using Lex;
+using System.Collections.Generic;
 
 namespace Parse
 {
+    public abstract class Parser
+    {
+        protected IEnumerator<Token> tokens;
+        public Parser(IEnumerator<Token> _tokens)
+        {
+            tokens = _tokens;
+        }
+
+        public abstract Node? Parse();
+        protected bool CheckTokenValue(string value)
+        {
+            return tokens.MoveNext() && tokens.Current.Value == value;
+        }
+        
+        protected bool CheckTokenType(TokenType tType)
+        {
+
+            return tokens.MoveNext() && tokens.Current.Type == tType;
+        }
+
+        protected bool CheckSequence(bool[] parseConditions)
+        {
+            foreach(var condition in parseConditions)
+            {
+                if (!condition) return false;
+            }
+            return true;
+        }
+    }
+
+    public class ConstantParser : Parser
+    {
+        public ConstantParser(IEnumerator<Token> _token) : base(_token)
+        {
+        }
+
+        public override Constant? Parse()
+        {
+            if (!CheckTokenType(TokenType.integerLiteral)) return null;            
+            return new Constant(tokens.Current.Value);
+        }
+    }
+
+    public class FunctionParser : Parser
+    {
+        public FunctionParser(IEnumerator<Token> _token) : base(_token)
+        {
+        }
+        
+        public override Function? Parse()
+        {
+            bool sequenceCheck = CheckSequence([
+                CheckTokenValue("int"),
+                CheckTokenType(TokenType.identifier),
+            ]);
+            if (!sequenceCheck) return null;
+            
+                string identifier = tokens.Current.Value;
+
+            sequenceCheck = CheckSequence([
+                CheckTokenType(TokenType.openParantheses),
+                CheckTokenType(TokenType.closedParentheses),
+                CheckTokenType(TokenType.openBrace)
+            ]);
+            if (!sequenceCheck) return null;
+            
+            Statement? statement = ParseStatement(tokens);
+            if (statement is null) return null;
+
+            if (!CheckTokenType(TokenType.closeBrace)) return null;
+            
+            return new Function(identifier, statement);
+        }
+
+    }
+
     public static class Parsers
     {
         private static bool CheckTokenValue(IEnumerator<Token> tokens, string value)
