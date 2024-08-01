c183f64fa3b9c8110191da7cf27550d459ec9036
jeremyhorwich <71720955+jeremyhorwich@users.noreply.github.com>
2024-08-01 11:42:30 -0500

    strip out coupling to other namespaces

--- Compiler/parser/ASTNodes.cs
+++ Compiler/parser/ASTNodes.cs
@@ -1,180 +1,74 @@
-using Lex;
-
-namespace Parse
+namespace Tree
 {
-    public abstract class Node
+    public interface IVisitor
     {
-        public abstract string Generate();
+        public T Visit<T>(Node node);
     }
 
-    public class Constant : Node
+    public abstract class Node
     {
-        private string integerLiteral;
-        public Constant(string _integerLiteral)
+    }
+
+    public class Function : Node 
+    {
+        public string identifier;
+        public Statement statement;
+
+        public Function(string _identifier, Statement _statement)
+        {
+            identifier = _identifier;
+            statement = _statement;
+        }
+
+        public T Accept<T>(IVisitor visitor)
+        {
+            return visitor.Visit<T>(this);
+        }
+    }
+
+    public abstract class Statement : Node
+    {
+        public virtual T Accept<T>(IVisitor visitor)
+        {
+            return visitor.Visit<T>(this);
+        }
+    }
+
+    public class ReturnStatement : Statement 
+    {
+        public Expression expression;
+
+        public ReturnStatement(Expression _expression)
+        {
+            expression = _expression;
+        }
+
+        public override T Accept<T>(IVisitor visitor)
+        {
+            return visitor.Visit<T>(this);
+        }
+    }
+
+    public abstract class Expression : Node
+    {
+        public T Accept<T>(IVisitor visitor)
+        {
+            return visitor.Visit<T>(this);
+        }
+    }
+
+    public class Constant : Statement 
+    {
+        public int integerLiteral;
+
+        public Constant(int _integerLiteral)
         {
             integerLiteral = _integerLiteral;
         }
 
-        public override string Generate()
+        public override T Accept<T>(IVisitor visitor)
         {
-            return new Instruction("movl", $"${integerLiteral}, %eax").Format();
-        }
-
-        public override string ToString()
-        {
-            return $"Int<{integerLiteral}>";
-        }
-    }
-
-    public class UnaryOperator : Node
-    {
-        private TokenType operation;
-        
-        public UnaryOperator(TokenType _operation)
-        {
-            operation = _operation;
-        }
-
-        public override string Generate()
-        {
-            string assembly = "";
-            switch (operation)
-            {
-                case TokenType.negation:
-                    assembly += new Instruction("neg", "%eax").Format();
-                    break;
-                case TokenType.bitwiseComplement:
-                    assembly += new Instruction("not", "%eax").Format();
-                    break;
-                case TokenType.logicalNegation: 
-                    assembly += new Instruction("cmpl", "%eax").Format();
-                    assembly += new Instruction("xor", "%eax, %eax").Format();
-                    assembly += new Instruction("sete", "%al");
-                    break;
-                default:
-                    throw new Exception("Bad operator in unary operator node (should be -,~, or !)");
-            }
-            return assembly;
-        }
-
-        public override string ToString()
-        {
-            return $"{operation}";
-        }
-    }
-
-    public class Expression : Node
-    {
-        private Constant? constant;
-        private UnaryOperator? unaryOperator;
-        private Expression? expression;
-
-        public Expression(Constant _constant)
-        {
-            constant = _constant;
-        }
-        
-        public Expression(UnaryOperator _unaryOperator, Expression _expression)
-        {
-            unaryOperator = _unaryOperator;
-            expression = _expression;
-        }
-
-        public override string Generate()
-        {
-            if (constant is not null) return constant.Generate();
-            if (unaryOperator is not null) return unaryOperator.Generate();
-            
-            return "";
-        }
-
-        public override string ToString()
-        {
-            return $"{constant}";
-        }
-    }
-
-    public class Return : Node
-    {
-        private Expression expression;
-
-        public Return(Expression _expression)
-        {
-            expression = _expression;
-        }
-
-        public override string Generate()
-        {
-            string assembly = "";
-            assembly += expression.Generate();
-            assembly += new Instruction("popq", "%rbp").Format();
-            assembly += new Instruction("ret", "").Format();
-            return assembly;
-        }
-
-        public override string ToString()
-        {
-            return $"\n\tRETURN {expression}";
-        }
-    }
-
-    public class Statement : Node
-    {
-        private Return? returnStatement;
-        public Statement(Return _returnStatement)
-        {
-            returnStatement = _returnStatement;
-        }
-
-        public override string Generate()
-        {
-            if (returnStatement is not null)
-            {
-                return returnStatement.Generate();
-            }
-            
-            return "";  //this make sense for when we have multiple ways of declaring an expression
-        }
-
-        public override string ToString()
-        {
-            if (returnStatement is not null)
-            {
-                return returnStatement.ToString(); 
-            }
-            return "";
-        }
-    }
-
-    public class Function : Node
-    {
-        private string identifier;
-        Statement statement;
-
-        public Function(string _identifier, Statement _Statement)
-        {
-            identifier = _identifier;
-            statement = _Statement;
-        }
-
-        public override string Generate()
-        {
-            string assembly = "";
-            assembly += new Instruction(".globl",identifier).Format();
-            assembly += $"\n{identifier}:";
-            assembly += new Instruction("pushq", "%rbp").Format();
-            assembly += new Instruction("movq", "%rsp, %rbp").Format();
-            assembly += statement.Generate();
-            return assembly;
-        }
-
-        public override string ToString()
-        {
-            string declaration = $"FUN INT {identifier}";
-            string parameters = "\n Params: ()";
-            string body = $"\n Body: {statement}";
-            
-            return declaration + parameters + body;
+            return visitor.Visit<T>(this);
         }
     }
 }
