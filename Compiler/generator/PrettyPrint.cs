9d013b2eb153341e3edefa9b0a7e7be0718f2ef2
jeremyhorwich <jeremyahorwich@gmail.com>
2024-08-05 12:57:36 -0500

    refactor for dynamic dispatch

--- Compiler/generator/PrettyPrint.cs
+++ Compiler/generator/PrettyPrint.cs
@@ -15,21 +15,39 @@ namespace Generator
             Console.WriteLine(Visit(program));;
         }
 
-        public string Visit(CProgram program) => $"\n{Visit(program.function)}";
+        public string Visit(CProgram program) => $"{Visit(program.function)}";
 
         public string Visit(Function function)
         {
-            return $"\n\t{function.identifier}{Visit(function.statement)}";
+            return $"\nFUNCTION {function.identifier}:{Visit(function.statement)}";
         }
 
-        public string Visit(Statement statement) => "";
+        public string Visit(Statement statement)
+        {
+            switch (statement)
+            {
+                case ReturnStatement returnStmt:
+                    return Visit(returnStmt);
+                default:
+                    throw new InvalidOperationException($"Unhandled statement type: {statement.GetType().Name}");
+            }
+        }
 
         public string Visit(ReturnStatement returnStatement)
         {
-            return $"\n\tRETURN {returnStatement.expression}";
+            return $"\n\tRETURN {Visit(returnStatement.expression)}";
         }
 
-        public string Visit(Expression expression) => "";
+        public string Visit(Expression expression)
+        {
+            switch (expression)
+            {
+                case Constant constant:
+                    return Visit(constant);
+                default:
+                    throw new InvalidOperationException($"Unhandled expression type: {expression.GetType().Name}");
+            }
+        }
 
         public string Visit(Constant constant)
         {
