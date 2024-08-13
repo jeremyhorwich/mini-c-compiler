e494c1ad9124233093ef31733762d04233e5bf66
jeremyhorwich <jeremyahorwich@gmail.com>
2024-08-13 17:10:02 -0500

    Create Environment.cs

---
+++ Compiler/generator/Environment.cs
@@ -0,0 +1,23 @@
+namespace Generator
+{
+    public class Environment
+    {
+        private Dictionary<string, Object> values = new Dictionary<string, Object>();
+
+        public void Define(string varName, Object value)
+        {
+            if (values.ContainsKey(varName))
+            {
+                throw new Exception($"Error: variable {varName} already declared elsewhere.");
+            }
+            values[varName] = value;
+        } 
+
+        public Object Get(string varName)
+        {
+            if (values.ContainsKey(varName)) return values[varName];
+            //TODO make this a parsing instead of compiling error
+            throw new Exception($"Error: variable {varName} does not exist");
+        }
+    }
+}
